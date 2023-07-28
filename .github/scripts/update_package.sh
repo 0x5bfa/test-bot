614c1453642555634bb3a4af2ba97435cb59b0c0
0x5BFA <62196528+0x5bfa@users.noreply.github.com>
2023-07-28 22:22:25 +0900

    Delete .github/scripts directory

--- .github/scripts/update_package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filename="../../../Package.appxmanifest"
-
-if [[ $search != "<Identity * Version=\"2.4.40.0\" />" && $replace != "<Identity * Version=\"$1\" />" ]]; then
-  sed -i "s/$search/$replace/" $filename
-fi
